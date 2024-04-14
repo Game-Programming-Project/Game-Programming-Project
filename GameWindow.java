ff1541a825ff44a4dcb4854584524c8405b06c8c
alliyah01 <alliyah.hosein@gmail.com>
2024-04-14 00:33:26 -0400

    Added GameApplication.java class

---
+++ GameWindow.java
@@ -0,0 +1,56 @@
+
+import javax.swing.*;			// need this for GUI objects
+import java.awt.*;			// need this for Layout Managers
+import java.awt.event.*;		// need this to respond to GUI events
+	
+public class GameWindow extends JFrame implements ActionListener, KeyListener, MouseListener{
+
+    public GameWindow(){
+
+    }
+
+    public void actionPerformed(ActionEvent e) {
+    }
+
+    // implement methods in KeyListener interface
+
+	public void keyPressed(KeyEvent e) {
+        
+	}
+
+	public void keyReleased(KeyEvent e) {
+
+	}
+
+	public void keyTyped(KeyEvent e) {
+
+	}
+
+
+	// implement methods in MouseListener interface
+
+	public void mouseClicked(MouseEvent e) {
+
+	}
+
+
+	public void mouseEntered(MouseEvent e) {
+	
+	}
+
+	public void mouseExited(MouseEvent e) {
+	
+	}
+
+	public void mousePressed(MouseEvent e) {
+	
+	}
+
+	public void mouseReleased(MouseEvent e) {
+	
+	}
+
+
+
+
+}
