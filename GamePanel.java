dbe6509bc894c3333871361cef7e823d5283b411
khadishaclarke <khadishaclarke.n@gmail.com>
2024-04-26 20:30:00 -0400

    added ladder images for each level

--- GamePanel.java
+++ GamePanel.java
@@ -45,7 +45,7 @@ public class GamePanel extends JPanel implements Runnable {
 
 	public GamePanel(GameWindow w) {
 
-		this.window = w; 
+		this.window = w;
 		characterSelected = false;
 
 		setLayout(new GridBagLayout());
@@ -132,12 +132,13 @@ public class GamePanel extends JPanel implements Runnable {
 				enemy.start();
 			enemy.update();
 
-			//if the enemy collides with player and the player has attacked, then the enemy takes damage
+			// if the enemy collides with player and the player has attacked, then the enemy
+			// takes damage
 			if (enemy.collidesWithPlayer(player) && player.attackRegistered() && enemy.isAlive()) {
 				System.out.println("enemy HIT for " + player.getAttackDamage() + " damage");
 				enemy.takeDamage(player.getAttackDamage());
 
-				if(!player.isInvincible())
+				if (!player.isInvincible())
 					player.takeDamage(enemy.getAttackDamage());
 			}
 
@@ -179,10 +180,12 @@ public class GamePanel extends JPanel implements Runnable {
 
 		if (background != null && player != null && !isPaused && direction != 99) {
 			if (!wouldCollide) { // if wouldn't collide with solid then move in the direction
-				int batMovement = background.move(direction); // check whether the bat can start/stop moving in a new direction
+				int batMovement = background.move(direction); // check whether the bat can start/stop moving in a new
+																// direction
 
 				player.setDirections(batMovement);
-				background.setDirections(player.move(direction)); // check if the bat is centred so the background can move
+				background.setDirections(player.move(direction)); // check if the bat is centred so the background can
+																	// move
 			}
 		}
 	}
@@ -241,7 +244,7 @@ public class GamePanel extends JPanel implements Runnable {
 		if (gameThread == null || !isRunning) {
 			// soundManager.playClip ("background", true);
 			createGameEntities();
-			levelInitializer.initLevelThree();
+			levelInitializer.initLevelOne();
 			gameThread = new Thread(this);
 			gameThread.start();
 		}
