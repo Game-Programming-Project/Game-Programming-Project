2e81060172c07a8655a314f7d76d264e6d060fbd
100levelz <j100levelz@gmail.com>
2024-04-26 20:28:42 -0400

    Fixed mushroom class movement, blow up animation and sounds

--- SoundManager.java
+++ SoundManager.java
@@ -34,6 +34,9 @@ public class SoundManager { // a Singleton class
 		clip = loadClip("sounds/Level1/mushroomWalk.wav"); // played when a mushroom walks
 		clips.put("mushroomWalk", clip);
 
+		clip = loadClip("sounds/Level1/mushroomBoom.wav"); // played when a mushroom explodes
+		clips.put("mushroomBoom", clip);
+
 		clip = loadClip("sounds/Level1/grasshopperJump.wav"); // played when a grasshopper jumps
 		clips.put("grasshopperJump", clip);
 
